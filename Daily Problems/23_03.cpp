731c720f88c564e505d7a22ba8e757b1ee574219
sanskrutikhedkar9 <sanskrutikhedkar09@gmail.com>
2024-03-23 11:48:17 +0530

    daily_commit

---
+++ Daily Problems/23_03.cpp
@@ -0,0 +1,57 @@
+/*143. Reorder List
+
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+*/
+
+#include<iostream>
+using namespace std;
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+class solution{
+    public:
+    ListNode* reverse(ListNode* head){
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* next=NULL;
+        while(curr!=NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    void reorderList(ListNode* head){
+        if(head!=NULL or head->next!=NULL or head->next->next!=NULL){
+            return ;
+        }
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=NULL and fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        ListNode* second=reverse(slow->next);
+        slow->next=NULL;
+        ListNode* first=head;
+        while(second){
+            ListNode* temp1=first->next;
+            ListNode* temp2=second->next;
+            first->next=second;
+            second->next=temp1;
+            first=temp1;
+            second=temp2;
+        }
+    }
+};
